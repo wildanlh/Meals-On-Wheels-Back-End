4f4c2547cbc812a9bbb24887e2c3f161e422aeae
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-02-01 07:33:52 +0700

    Initial project

---
+++ src/main/java/com/lithan/mow/MealsOnWheelsBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.lithan.mow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealsOnWheelsBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealsOnWheelsBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lithan/mow/MealsOnWheelsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lithan.mow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealsOnWheelsBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
